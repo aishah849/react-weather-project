27e3d52ba4aedc7b1e87f3545e6c3f92fbbb8ac9
aishah849 <aishahalrashid@gmail.com>
2022-07-19 00:52:30 +0800

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Aishah Al-Rashid and it is {""}
+        <a
+          href="https://github.com/aishah849/react-weather-project"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
