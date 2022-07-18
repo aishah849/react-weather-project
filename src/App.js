911ce71981906fc69c2721f5c979ae1144dbca00
aishah849 <aishahalrashid@gmail.com>
2022-07-19 01:04:17 +0800

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Aishah Al-Rashid and it is {""}
-        <a
-          href="https://github.com/aishah849/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Aishah Al-Rashid and it is {""}
+          <a
+            href="https://github.com/aishah849/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
